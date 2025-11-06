246560a48cfb21e431cee8f731e9fef3fc91d1ca
KevinUdz <kevinuruel22@gmail.com>
2025-11-05 23:27:39 -0600

    Add files via upload

---
+++ script_iggly.js
@@ -0,0 +1,23 @@
+// Igglybuff (forma base)
+function mostrarIgglybuff() {
+    document.getElementById("personaje").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/174.png";
+    document.body.style.backgroundImage = "url('fondo_iggly.jpeg')";
+}
+
+// Jigglypuff
+function mostrarJigglypuff() {
+    document.getElementById("personaje").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png";
+    document.body.style.backgroundImage = "url('fondo_jiggly.jpg')";
+}
+
+// Wigglytuff
+function mostrarWigglytuff() {
+    document.getElementById("personaje").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/40.png";
+    document.body.style.backgroundImage = "url('fondo_wiggly.jpg')";
+}
+
+// Igglybuff shiny (versión especial)
+function mostrarShiny() {
+    document.getElementById("personaje").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/174.png";
+    document.body.style.backgroundImage = "url('fondo_shiny.jpg')";
+}
